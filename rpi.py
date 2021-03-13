84ce5050423dffc607db1913aace26e69910555a
BingJiaChen <b07901088@ntu.edu.tw>
2021-03-13 20:37:01 +0800

    rpi

--- rpi.py
+++ rpi.py
@@ -1,16 +1,21 @@
 from picamera import PiCamera
 from time import sleep
+import RPi.GPIO as GPIO
 import requests
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.IN)
 camera = PiCamera()
-# camera.rotation = 180
 camera.resolution = (800, 600) # (3280, 2464)
-# camera.start_preview()
-# sleep(3)
-camera.capture("./img.jpg")
-# camera.stop_preview()
-
-url = 'https://khaos.tw/uploaded/guest'
-files = {'myFile': open('./img.jpg', 'rb')}
-response = requests.request("POST", url, files=files)
-print(response.text)
+while True:
+    inputValue = GPIO.input(11)
+    if inputValue==False:
+        camera.start_preview()
+        sleep(1)
+        camera.capture('./img.jpg')
+        camera.stop_preview()
+        url = 'https://khaos.tw/uploaded/guest'
+        files = {'myFile': open('./img.jpg', 'rb')}
+        response = requests.request("POST", url, files=files)
+        print(response.text)
+        sleep(0.5)
