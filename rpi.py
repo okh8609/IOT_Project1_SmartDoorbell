c5dca0d77aedc1750f795f83199fe0d6836af89f
BingJiaChen <b07901088@ntu.edu.tw>
2021-03-14 18:46:45 +0800

    rpi

--- rpi.py
+++ rpi.py
@@ -5,6 +5,7 @@ import requests
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.IN)
+GPIO.setup(13,GPIO.OUT)
 camera = PiCamera()
 camera.resolution = (800, 600) # (3280, 2464)
 while True:
@@ -21,5 +22,9 @@ while True:
         sleep(0.5)
     
     r = requests.get("https://khaos.tw/door_status")
-    print(r.content)
-    sleep(1)
+    if bool(r.content)==True:
+        GPIO.output(13,GPIO.LOW)
+        sleep(0.25)
+    else:
+        GPIO.output(13,GPIO.HIGH)
+        sleep(0.25)
